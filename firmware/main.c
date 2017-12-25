2f2c24dcfe6c9b52cfd3b209084a6c85a331082a
Ondrej Hejda <ondrejh.ck@email.cz>
2017-12-25 14:41:52 +0100

    and code

--- firmware/main.c
+++ firmware/main.c
@@ -1,25 +1,17 @@
 //******************************************************************************
-// Lunar clock timer for MSP430 launchpad
+// Police light timer for MSP430 launchpad
 //
 //
 // author: Ondrej Hejda
-// date:   19.12.2017
+// date:   25.12.2017
 //
 // hardware: MSP430G2231 (launchpad)
 //
 //                MSP4302231
 //             -----------------
 //         /|\|                 |
-//          | |             XTAL|<---> 32.768kHz quartz (realtime clock)
-//          --|RST              |
-//            |                 |
-//            |             P1.6|----> COIL
-//            |             P1.7|----> COIL
-//            |                 |
-//            |             P1.3|-------  POTENTIOMETER (10k)
-//            |                 |      \/
-//            |             P1.4|----[XXXX]----
-//            |             P1.5|--------------
+//          | |                 |
+//          --|RST          P1.6|----> LED
 //            |                 |
 //
 //******************************************************************************
@@ -32,69 +24,61 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define COIL_INIT() do{P1DIR|=0xC0;P1OUT&=~0xC0;}while(0)
-#define COIL_A_ON() do{P1OUT|=0x40;}while(0)
-#define COIL_B_ON() do{P1OUT|=0x80;}while(0)
-#define COIL_OFF() do{P1OUT&=~0xC0;}while(0)
+#define LED_INIT() do{P1DIR|=0x40;P1OUT&=~0x40;}while(0)
+#define LED_ON() do{P1OUT|=0x40;}while(0)
+#define LED_OFF() do{P1OUT&=~0x40;}while(0)
 
-#define TICKS_MIN 384
-#define TICKS_AVG 448
-#define TICKS_MAX 512
+#define FLASHES 6
+#define FLASH_ON_TIME 7
+#define FLASH_OFF_TIME 7
+#define PAUSE 40
 
-void rtc_timer_init(void)
-{
-	CCTL0 = CCIE; // CCR0 interrupt enabled
-	CCR0 = 31;	  // f = 32768 / 8(ID_3) / 32(CCR0+1) = 128Hz
-	//CCR0 = 63;	  // f = 32768 / 8(ID_3) / 64(CCR0+1) = 64Hz
-	//CCR0 = 127;	  // f = 32768 / 8(ID_3) / 128(CCR0+1) = 32Hz
-	//CCR0 = 511;	  // f = 32768 / 8(ID_3) / 512(CCR0+1) = 8Hz
-	//CCR0 = 1023;	  // f = 32768 / 8(ID_3) / 1024(CCR0+1) = 4Hz
-	//CCR0 = 2047;	  // f = 32768 / 8(ID_3) / 2048(CCR0+1) = 2Hz
-	TACTL = TASSEL_1 + ID_3 + MC_1; // ACLK, /8, upmode
-}
+#define TICK_TIME 10000
 
-void analog_init(void)
-{
-    P1DIR |= 0x30; P1OUT&=~0x30;
-    ADC10CTL0 = ADC10SHT_2 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
-    ADC10CTL1 = INCH_3;                         // input A3
-    ADC10AE0 |= 0x08;                           // PA.3 ADC option select
-}
+#define SLEEP(x) do{sleep_time=x;__bis_SR_register(CPUOFF + GIE);}while(0)
+#define WAKE() do{__bic_SR_register_on_exit(CPUOFF);}while(0)
+
+volatile uint16_t sleep_time = 0;
 
 // leds and dco init
-void board_init(void)
+void init(void)
 {
 	// oscillator
 	BCSCTL1 = CALBC1_1MHZ;		// Set DCO
 	DCOCTL = CALDCO_1MHZ;
 
-	COIL_INIT();
-}
+    // led output
+	LED_INIT();
 
-volatile uint16_t ticks = TICKS_AVG;
+    // timer    
+    CCTL0 = CCIE;                             // CCR0 interrupt enabled
+	CCR0 = TICK_TIME;
+    TACTL = TASSEL_2 + MC_2; // SMCLK, contmode
+}
 
 // main program body
 int main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD;	// Stop WDT
 
-	board_init(); // init dco and leds
-	rtc_timer_init(); // init 32kHz clock timer
-    analog_init();
+	init();
 
 	while(1)
 	{
-        __bis_SR_register(CPUOFF + GIE); // enter sleep mode
-        // switch on potentiometer
-        P1OUT |= 0x20;
-        __delay_cycles(200);
-        // start conversion
-        ADC10CTL0 |= ENC + ADC10SC;
-        __bis_SR_register(CPUOFF + GIE);
-        // get value
-        ticks = TICKS_MIN + (ADC10MEM>>3);
-        // switch it off
-        P1OUT &= ~0x20;
+  	    LED_ON();
+		SLEEP(FLASH_ON_TIME);
+		
+		int i;
+		for (i=1;i<FLASHES;i++) {
+			LED_OFF();
+			SLEEP(FLASH_OFF_TIME);
+		
+			LED_ON();
+			SLEEP(FLASH_ON_TIME);
+		}
+		
+		LED_OFF();
+        SLEEP(PAUSE);
 	}
 
 	return -1;
@@ -106,51 +90,9 @@ int main(void)
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A (void)
 {
-    static uint16_t cnt = 0;
-    static uint16_t coil_on = 0;
-
-    /*// standard clock (16Hz tick)
-	if ((cnt&0x07)==0) {
-		if (cnt&0x08)
-			COIL_A_ON();
-		else
-			COIL_B_ON();
-        coil_on = 2;
-	}*/
-    
-    // lunar clock
-    static uint16_t next = TICKS_AVG;
-    if (cnt==next) {
-        static bool A = true;
-        if (A)
-            COIL_A_ON();
-        else
-            COIL_B_ON();
-        //LED_GREEN_ON();
-        A = !A;
-        coil_on = 2;
-        next += ticks;
-    }
-    
-    // coil zeroing
-	else {
-        if (coil_on) {
-            //LED_GREEN_OFF();
-            coil_on--;
-            if (coil_on==0)
-                COIL_OFF();
-        }   
-	}
-
-    if (cnt==0)
-        __bic_SR_register_on_exit(CPUOFF); // Clear CPUOFF bit from 0(SR)
-
-	cnt++;
-}
-
-// ADC10 interrupt service routine
-#pragma vector=ADC10_VECTOR
-__interrupt void ADC10_ISR(void)
-{
-    __bic_SR_register_on_exit(CPUOFF); // Clear CPUOFF bit from 0(SR)
+	CCR0 += TICK_TIME; // Add Offset to CCR0
+	if (sleep_time>0)
+		sleep_time--;
+	if (sleep_time==0)
+		WAKE();
 }
