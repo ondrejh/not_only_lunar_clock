7082a7d1ea2ec2f7d2eaa98ebdf60274c55d24cc
Ondrej Hejda <ondrejh.ck@email.cz>
2018-01-06 10:45:59 +0100

    basic function

--- fw/main.c
+++ fw/main.c
@@ -7,14 +7,14 @@
 //
 // hardware: MSP430G2231 (launchpad)
 //
-//                MSP4302231
+//                MSP430G2231
 //             -----------------
 //         /|\|                 |
-//          | |             P1.0|----> MOTOR BW
-//          --|RST          P1.1|----> MOTOR FW
+//          | |             P1.2|----> MOTOR BW
+//          --|RST          P1.3|----> MOTOR FW
 //            |                 |
-//            |             P1.2|----> LED_L
-//            |             P1.3|----> LED_R
+//            |             P1.0|----> LED_L
+//            |             P1.1|----> LED_R
 //            |                 |
 //            |             P1.4|<---- CH2 (throtle)
 //            |             P1.5|<---- CH3 (light)
@@ -30,11 +30,29 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define MOTOR_FW() do{P1OUT=(P1OUT&0xFC)|0x02;}while(0)
-#define MOTOR_BW() do{P1OUT=(P1OUT&0xFC)|0x01;}while(0)
-#define MOTOR_STOP() do{P1OUT&=0xFC;}while(0)
-#define LED_ON() do{P1OUT|=0x04;}while(0)
-#define LED_OFF() do{P1OUT&=0xF3;}while(0)
+#define LED_ON() do{P1OUT|=0x01;}while(0)
+#define LED_OFF() do{P1OUT&=0xFC;}while(0)
+
+#define IN1_HIGH() do{P1OUT|=0x04;}while(0)
+#define IN1_LOW() do{P1OUT&=~0x04;}while(0)
+#define IN2_HIGH() do{P1OUT|=0x08;}while(0)
+#define IN2_LOW() do{P1OUT&=~0x08;}while(0)
+
+#define CH2_M (1<<4)
+#define CH3_M (1<<5)
+
+#define GREEN_ON() do{P1OUT|=0x40;}while(0)
+#define GREEN_OFF() do{P1OUT&=~0x40;}while(0)
+#define GREEN_SWAP() do{P1OUT^=0x40;}while(0)
+
+#define THOLD 60
+#define CENTER 1500
+#define MAX 2200
+#define MIN 800
+
+#define PWM_MAX 1023
+
+uint16_t pwm;
 
 // leds and dco init
 void board_init(void)
@@ -44,11 +62,48 @@ void board_init(void)
 	DCOCTL = CALDCO_1MHZ;
 
     // led and motor outputs
-	P1DIR |= 0x0F; P1OUT &= 0xF0;
+	P1DIR |= 0x4F; P1OUT &= 0xB0;
     // servo signal input
     P1DIR &= 0xCF;
 
-    LED_ON();
+    // start timer
+    TACTL = TASSEL_2 + MC_2; // SMCLK, continuous
+
+    LED_OFF();
+}
+
+// channel 2
+void ch2(uint16_t v)
+{
+    if ((v<MIN) | (v>MAX)) return;
+
+    uint16_t p;
+
+    if (v < (CENTER - THOLD)) {
+        p = CENTER - v;
+        p <<= 1;
+        pwm = p;
+        IN2_HIGH();
+    }
+    else if (v > (CENTER + THOLD)) {
+        p = v - CENTER;
+        p <<= 1;
+        pwm = PWM_MAX - p;
+        IN2_LOW();
+    }
+    else {
+        pwm = PWM_MAX;
+        IN2_LOW();
+    }
+}
+
+// channel 3
+void ch3(uint16_t v)
+{
+    if ((v<MIN) | (v>MAX)) return;
+    
+    if (v<1500) LED_OFF();
+    else LED_ON();
 }
 
 // main program body
@@ -58,9 +113,43 @@ int main(void)
 
 	board_init(); // init dco and leds
 
+    // wait both servo inputs are low
+    while (P1IN&(CH2_M|CH3_M)) {};
+
+    uint8_t in_last = 0;
+
+    uint16_t ch2_start = 0;
+    uint16_t ch3_start = 0;
+
 	while(1)
 	{
-        __bis_SR_register(CPUOFF + GIE); // enter sleep mode
+        uint16_t now = TAR;
+        uint8_t in_now = P1IN;
+        
+        uint8_t in_changes = in_now^in_last;
+        uint8_t in_goes_up = in_changes&in_now;
+        uint8_t in_goes_down = in_changes&in_last;
+        
+        // get channel 3 value
+        if (in_goes_up & CH3_M)
+            ch3_start = now;
+        else if (in_goes_down & CH3_M)
+            ch3(now-ch3_start);
+
+        // get channel 2 value
+        if (in_goes_up & CH2_M)
+            ch2_start = now;
+        else if (in_goes_down & CH2_M)
+            ch2(now-ch2_start);
+
+        // do some pwm
+        if (pwm<(now&PWM_MAX))
+            IN1_HIGH();
+        else
+            IN1_LOW();
+
+        // save input for next round
+        in_last = in_now;
 	}
 
 	return -1;
