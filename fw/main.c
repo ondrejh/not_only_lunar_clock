1c8c094827ae693adb82381c1cd3b299d2221a44
Ondrej Hejda <ondrejh.ck@email.cz>
2018-02-08 21:49:28 +0100

    beep at least

--- fw/main.c
+++ fw/main.c
@@ -1,23 +1,20 @@
 //******************************************************************************
-// RC signal DC motor drive for MSP430 launchpad
+// LiPo voltage guard using MSP430 launchpad
 //
 //
 // author: Ondrej Hejda
-// date:   6.1.2018
+// date:   8.2.2018
 //
 // hardware: MSP430G2231 (launchpad)
 //
 //                MSP430G2231
 //             -----------------
 //         /|\|                 |
-//          | |             P1.0|----> LED IN1
-//          --|RST          P1.1|----> LED IN2
+//          | |             P1.0|----> RED LED
+//          --|RST          P1.6|----> GREEN LED
 //            |                 |
-//            |             P1.2|----> MOTOR IN1
-//            |             P1.3|----> MOTOR IN2
-//            |                 |
-//            |             P1.4|<---- CH2 (throtle)
-//            |             P1.5|<---- CH3 (light)
+//            |             P1.4|----> BEEP +
+//            |             P1.5|----> BEEP -
 //            |                 |
 //
 //******************************************************************************
@@ -30,29 +27,16 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define LED_ON() do{P1OUT|=0x01;}while(0)
-#define LED_OFF() do{P1OUT&=0xFC;}while(0)
+#define GREEN_ON() do{P1OUT|=0x40;}while(0)
+#define GREEN_OFF() do{P1OUT&=~0x40;}while(0)
+#define RED_ON() do{P1OUT|=0x01;}while(0)
+#define RED_OFF() do{P1OUT&=~0x01;}while(0)
 
-#define IN1_HIGH() do{P1OUT|=0x04;}while(0)
-#define IN1_LOW() do{P1OUT&=~0x04;}while(0)
-#define IN2_HIGH() do{P1OUT|=0x08;}while(0)
-#define IN2_LOW() do{P1OUT&=~0x08;}while(0)
+#define BEEP_OFF() do{P1OUT&=~0x30;}while(0)
+#define BEEP_P() do{BEEP_OFF();P1OUT|=0x10;}while(0)
+#define BEEP_N() do{BEEP_OFF();P1OUT|=0x20;}while(0);
 
-#define CH2_M (1<<4)
-#define CH3_M (1<<5)
-
-#define GREEN_OFF() do{P1OUT|=0x40;}while(0)
-#define GREEN_ON() do{P1OUT&=~0x40;}while(0)
-#define GREEN_SWAP() do{P1OUT^=0x40;}while(0)
-
-#define THOLD 70
-#define CENTER 1500
-#define MAX 2200
-#define MIN 800
-
-#define PWM_MAX 1023
-
-uint16_t pwm;
+#define BEEP_PERIOD 133
 
 // leds and dco init
 void board_init(void)
@@ -62,48 +46,10 @@ void board_init(void)
 	DCOCTL = CALDCO_1MHZ;
 
     // led and motor outputs
-	P1DIR |= 0x4F; P1OUT &= 0xB0;
-    // servo signal input
-    P1DIR &= 0xCF;
+	P1DIR |= 0x71; P1OUT &= 0x00;
 
     // start timer
     TACTL = TASSEL_2 + MC_2; // SMCLK, continuous
-
-    LED_OFF();
-}
-
-// channel 2
-void ch2(uint16_t v)
-{
-    if ((v<MIN) | (v>MAX)) return;
-
-    uint16_t p;
-
-    if (v < (CENTER - THOLD)) {
-        p = CENTER - v;
-        p <<= 1;
-        pwm = p;
-        IN2_HIGH();
-    }
-    else if (v > (CENTER + THOLD)) {
-        p = v - CENTER;
-        p <<= 1;
-        pwm = PWM_MAX - p;
-        IN2_LOW();
-    }
-    else {
-        pwm = PWM_MAX;
-        IN2_LOW();
-    }
-}
-
-// channel 3
-void ch3(uint16_t v)
-{
-    if ((v<MIN) | (v>MAX)) return;
-    
-    if (v<CENTER) LED_OFF();
-    else LED_ON();
 }
 
 // main program body
@@ -113,75 +59,29 @@ int main(void)
 
 	board_init(); // init dco and leds
 
+    uint16_t last = 0;
     while (1) {
-        // wait both servo inputs are low
-        while (P1IN&(CH2_M|CH3_M)) {};
-
-        GREEN_ON();
-
-        uint8_t in_last = 0;
-
-        uint16_t ch2_start = 0;
-        uint16_t ch3_start = 0;
-
-	    while(1) {
-            uint16_t now = TAR;
-            uint8_t in_now = P1IN;
-            
-            uint8_t in_changes = in_now^in_last;
-            uint8_t in_goes_up = in_changes&in_now;
-            uint8_t in_goes_down = in_changes&in_last;
-            
-            // get channel 3 value
-            if (in_goes_up & CH3_M)
-                ch3_start = now;
-            else if (in_goes_down & CH3_M)
-                ch3(now-ch3_start);
-
-            // get channel 2 value
-            if (in_goes_up & CH2_M)
-                ch2_start = now;
-            else if (in_goes_down & CH2_M)
-                ch2(now-ch2_start);
-
-            // do some pwm
-            if (pwm<(now&PWM_MAX))
-                IN1_HIGH();
-            else
-                IN1_LOW();
-
-            // save input for next round
-            in_last = in_now;
-
-            // if no servo signal switch off
-            if ((now-ch3_start) > 50000)
+        uint16_t now = TAR;
+        if ((now-last)>=BEEP_PERIOD) {
+            static int cnt;
+            cnt ++;
+            cnt &= 3;
+            switch (cnt) {
+            case 0:
+                BEEP_OFF();
                 break;
-	    }
-
-        GREEN_OFF();
-
-        LED_OFF();
-        IN1_LOW();
-        IN2_LOW();
-
-        ch2_start = TAR;
-        ch3_start = 0;
-
-        while (!(P1IN&CH3_M)) {
-            uint16_t now = TAR;
-            if ((now-ch2_start)>10000) {
-                ch2_start = now;
-                ch3_start++;
-                if (ch3_start>=200) {
-                    LED_ON();
-                    GREEN_ON();
-                    ch3_start = 0;
-                }
-                else {
-                    GREEN_OFF();
-                    LED_OFF();
-                }
+            case 1:
+                BEEP_P();
+                break;
+            case 2:
+                BEEP_OFF();
+                break;
+            case 3:
+                BEEP_N();
+                break;
             }
+            last += BEEP_PERIOD;
+            P1OUT ^= 0x10;
         }
     }
 
