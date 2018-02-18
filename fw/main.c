ae04cde0861e9f6c184ff5c547f2a3f836ca20f7
Ondrej Hejda <ondrejh.ck@email.cz>
2018-02-18 20:46:19 +0100

    adc reading, signalisation, switch off when low power

--- fw/main.c
+++ fw/main.c
@@ -30,11 +30,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define RED_LED_OFF() do{P1OUT|=0x20;}while(0)
-#define RED_LED_ON() do{P1OUT&=~0x20;}while(0)
+#define RED_OFF() do{P1OUT|=0x20;}while(0)
+#define RED_ON() do{P1OUT&=~0x20;}while(0)
+#define RED_SWAP() do{P1OUT^=0x20;}while(0)
 
-#define GREEN_LED_OFF() do{P1OUT|=0x40;}while(0)
-#define GREEN_LED_ON() do{P1OUT&=~0x40;}while(0)
+#define GREEN_OFF() do{P1OUT|=0x40;}while(0)
+#define GREEN_ON() do{P1OUT&=~0x40;}while(0)
+#define GREEN_SWAP() do{P1OUT^=0x40;}while(0)
 
 #define IN1_HIGH() do{P1OUT|=0x10;}while(0)
 #define IN1_LOW() do{P1OUT&=~0x10;}while(0)
@@ -43,12 +45,27 @@
 
 #define CH2_M (1<<0)
 
-#define THOLD 70
+#define THOLD 80
 #define CENTER 1500
 #define MAX 2200
 #define MIN 800
 
 #define PWM_MAX 1023
+
+#define DIV_HIGH 7.5
+#define DIV_LOW 4.7
+#define ADC_REF 3.3
+#define ADC_MAX 1023
+#define ADC_SAMPLES 8
+
+#define ADC_LEVEL(x) (uint16_t)(x/(DIV_HIGH+DIV_LOW)*DIV_LOW/ADC_REF*ADC_MAX*ADC_SAMPLES)
+
+#define ADC_CHARGED ADC_LEVEL(8.0)
+#define ADC_OK ADC_LEVEL(7.6)
+#define ADC_CRITICAL ADC_LEVEL(6.6)
+
+#define BLINK_SLOW 50000
+#define BLINK_FAST 20000
 
 uint16_t pwm;
 
@@ -61,14 +78,25 @@ void board_init(void)
 
     // led and motor outputs
 	P1DIR |= 0x78; P1OUT &= ~0x78;
+
     // servo signal input
     P1DIR &= ~0x01;
 
     // start timer
     TACTL = TASSEL_2 + MC_2; // SMCLK, continuous
 
-    GREEN_LED_ON();
-    RED_LED_ON();
+    // init ADC
+    ADC10CTL0 = ADC10SHT_2 + ADC10ON; // ADC10ON
+    ADC10CTL1 = INCH_1; // input A1
+    ADC10AE0 |= 0x02; // PA.1 ADC option select
+
+    // leds on
+    GREEN_ON();
+    RED_ON();
+
+    // switch motor off
+    pwm = PWM_MAX;
+    IN2_LOW();
 }
 
 // channel 2
@@ -96,6 +124,25 @@ void ch2(uint16_t v)
     }
 }
 
+// adc input
+uint8_t use_value(uint16_t val)
+{
+    if (val<ADC_CRITICAL)
+        return 0;
+    if (val<ADC_OK)
+        return 1;
+    if (val<ADC_CHARGED)
+        return 2;
+    return 3;
+}
+
+bool blink_div(void) {
+    static uint8_t div = 0;
+    div ++;
+    div &= 0x7;
+    return (div==0);
+}
+
 // main program body
 int main(void)
 {
@@ -103,54 +150,91 @@ int main(void)
 
 	board_init(); // init dco and leds
 
-    while (1) {
-        // wait servo input is low
-        while (P1IN&(CH2_M)) {};
+    uint8_t in_last = 0;
+    uint16_t ch2_start = P1IN;
 
-        RED_LED_OFF();
-        GREEN_LED_ON();
+    uint8_t adc_state = 0;
+    uint16_t adc_value = 0;
+    uint8_t adc_counter = 0;
+    uint16_t adc_timer = 0;
 
-        uint8_t in_last = 0;
+    uint8_t level = 1;
 
-        uint16_t ch2_start = P1IN;
-
-	    while(1) {
-            uint16_t now = TAR;
-            uint8_t in_now = P1IN;
-            
-            uint8_t in_changes = in_now^in_last;
-            uint8_t in_goes_up = in_changes&in_now;
-            uint8_t in_goes_down = in_changes&in_last;
-            
-            // get channel 2 value
-            if (in_goes_up & CH2_M)
-                ch2_start = now;
-            else if (in_goes_down & CH2_M)
+    while(1) {
+        uint16_t now = TAR;
+        uint8_t in_now = P1IN;
+        
+        uint8_t in_changes = in_now^in_last;
+        uint8_t in_goes_up = in_changes&in_now;
+        uint8_t in_goes_down = in_changes&in_last;
+        
+        // get channel 2 value
+        if (in_goes_up & CH2_M)
+            ch2_start = now;
+        else if (in_goes_down & CH2_M)
+            if (level>0) 
                 ch2(now-ch2_start);
 
-            // do some pwm
-            if (pwm<(now&PWM_MAX))
-                IN1_HIGH();
-            else
-                IN1_LOW();
-
-            // save input for next round
-            in_last = in_now;
-
-            // if no servo signal switch off
-            if ((now-ch2_start) > 50000)
-                break;
-	    }
-
-        GREEN_LED_OFF();
-
-        IN1_LOW();
-        IN2_LOW();
-
-        ch2_start = TAR;
-
-        while (!(P1IN&CH2_M)) {
+        // switch off when battery is low
+        if (level==0) {
+            pwm = PWM_MAX;
+            IN2_LOW();
         }
+
+        // do some pwm
+        if (pwm<(now&PWM_MAX))
+            IN1_HIGH();
+        else
+            IN1_LOW();
+
+        // adc reading
+        switch (adc_state) {
+        case 0:
+            if ((now-adc_timer) > 50000) {
+                ADC10CTL0 |= ENC + ADC10SC; // Sampling and conversion start
+                adc_timer = now;
+                adc_state ++;
+            }
+            break;
+        case 1:
+            if (!(ADC10CTL1 & ADC10BUSY)) {
+                adc_value += ADC10MEM;
+                adc_counter ++;
+                if (adc_counter >= ADC_SAMPLES)
+                    adc_state++;
+                else
+                    adc_state=0;
+            }
+            break;
+        case 2:
+            level = use_value(adc_value);
+            adc_counter = 0;
+            adc_value = 0;
+            adc_state = 0;
+            // blinking
+            switch (level) {
+            case 0:
+                GREEN_OFF();
+                RED_SWAP();
+                break;
+            case 1:
+                GREEN_OFF();
+                RED_ON();
+                break;
+            case 2:
+                GREEN_ON();
+                RED_OFF();
+                break;
+            case 3:
+                GREEN_SWAP();
+                RED_OFF();
+                break;
+            }
+            break;
+        }
+
+        // save input for next round
+        in_last = in_now;
     }
 
 	return -1;
